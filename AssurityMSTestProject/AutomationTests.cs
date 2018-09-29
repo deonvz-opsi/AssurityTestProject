6396a2f41b57f1eed9942bfff1d56766a0e77230
Deon van Zyl <075deon@gmail.com>
2018-09-29 13:56:52 +0200

    Refactor code: Create a combined test Create a global HTTP client Improve readability and add code comments for methods and tests

--- AssurityMSTestProject/AutomationTests.cs
+++ AssurityMSTestProject/AutomationTests.cs
@@ -1,13 +1,4 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json;
-using System.Threading;
 using ProductConsoleApp;
 
 namespace AssurityMSTestProject
@@ -15,26 +6,58 @@ namespace AssurityMSTestProject
     [TestClass]
     public class AutomationTests
     {
-        
+        /// <summary>
+        /// I strongly advise against combining three acceptance criteria in to an automated test as per the assignment criteria. 
+        /// For this reason I have created a combined test as well as three separate tests. One for each acceptance criteria.
+        /// 
+        /// Automated test that verifies three acceptance criteria:
+        /// Name = "Carbon credits"
+        /// CanRelist = true
+        /// The Promotions element with Name = "Gallery" has a Description that contains the text "2x large image"
+        /// </summary>
+        [TestMethod]
+        public void VerifyThreeCriteria()
+        {
+            //Create a variable that contains the Category name
+            var CategoryName = Program.GetCategory().GetAwaiter().GetResult();
+            //Create a list of Promotion JSON objects
+            var promotions = Program.GetPromotionList().GetAwaiter().GetResult();
+            //Get the boolean value of Category CanRelist item
+            bool CategoryCanRelist = Program.GetCanRelistStatus().GetAwaiter().GetResult();
+
+            //Verify the Name of the Category JSON object
+            Assert.IsTrue(CategoryName.Equals("Carbon credits"), "Product name is not equal to \"Carbon credits\"");
+            //Verify the boolean "true status" of the Category CanRelist status
+            Assert.IsTrue(CategoryCanRelist);
+            //Verify the string content "2x large image" in the Promotion item with Name "Gallery"
+            foreach (var item in promotions)
+            {
+                if (item.Name.Equals("Gallery"))
+                {
+                    Assert.IsTrue(item.Description.ToString().Contains("2x larger image"), "Gallery description varification has failed");
+                }
+            }
+        }
+        //Verify the Name of the Category JSON object
         [TestMethod]
         public void VerifyNameCarbonCredits()
         {
-            var ProductName = Program.GetProductName().GetAwaiter().GetResult();
-            Assert.IsTrue(ProductName.Equals("Carbon credits"), "Product name is not equal to \"Carbon credits\"");
+            var CategoryName = Program.GetCategory().GetAwaiter().GetResult();
+            Assert.IsTrue(CategoryName.Equals("Carbon credits"), "Product name is not equal to \"Carbon credits\"");
         }
-
+        //Verify the boolean "true status" of the Category CanRelist status
         [TestMethod]
         public void VerifyCanRelistTrue()
         {
-            bool ProductNameRelist = Program.GetCanRelistStatus().GetAwaiter().GetResult();
-            Assert.IsTrue(ProductNameRelist);
+            bool CategoryCanRelist = Program.GetCanRelistStatus().GetAwaiter().GetResult();
+            Assert.IsTrue(CategoryCanRelist);
         }
-
+        //Verify the string content "2x large image" in the Promotion item with Name "Gallery"
         [TestMethod]
         public void VerifyPromotionText()
         {
-            var items = Program.GetPromotionList().GetAwaiter().GetResult();
-            foreach (var item in items)
+            var promotions = Program.GetPromotionList().GetAwaiter().GetResult();
+            foreach (var item in promotions)
             {
                 if(item.Name.Equals("Gallery"))
                 {
@@ -42,5 +65,6 @@ namespace AssurityMSTestProject
                 }
             }
         }
+        
     }
 }
--- ProductConsoleApp/Program.cs
+++ ProductConsoleApp/Program.cs
@@ -1,23 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json;
-using System.Threading;
 
 namespace ProductConsoleApp
 {
-    public class JsonObjects
+    /// <summary>
+    /// JSON object of a Category item
+    /// </summary>
+    public class Category
     {
-        public static void test()
-        {
-            Console.WriteLine("Hello world");
-        }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public bool CanListAuctions { get; set; }
+        public bool CanListClassifieds { get; set; }
+        public bool CanRelist { get; set; }
+        public string LegalNotice { get; set; }
+        public int DefaultDuration { get; set; }
+        public List<int> AllowedDurations { get; set; }//List of AllowedDurations
+        public Fees Fees { get; set; }//Fees object
+        public int FreePhotoCount { get; set; }
+        public int MaximumPhotoCount { get; set; }
+        public bool IsFreeToRelist { get; set; }
+        public List<Promotion> Promotions { get; set; }//List of Promotion items contained in category
+        public List<object> EmbeddedContentOptions { get; set; }
+        public int MaximumTitleLength { get; set; }
+        public int AreaOfBusiness { get; set; }
+        public int DefaultRelistDuration { get; set; }
+
     }
+    /// <summary>
+    /// JSON object of a Promotion item which is a child of Category
+    /// </summary>
     public class Promotion
     {
         public int Id { get; set; }
@@ -28,28 +42,9 @@ namespace ProductConsoleApp
         public double? OriginalPrice { get; set; }
         public bool? Recommended { get; set; }
     }
-    public class ProductName
-    {
-        public int CategoryId { get; set; }
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public bool CanListAuctions { get; set; }
-        public bool CanListClassifieds { get; set; }
-        public bool CanRelist { get; set; }
-        public string LegalNotice { get; set; }
-        public int DefaultDuration { get; set; }
-        public List<int> AllowedDurations { get; set; }
-        public Fees Fees { get; set; }
-        public int FreePhotoCount { get; set; }
-        public int MaximumPhotoCount { get; set; }
-        public bool IsFreeToRelist { get; set; }
-        public List<Promotion> Promotions { get; set; }
-        public List<object> EmbeddedContentOptions { get; set; }
-        public int MaximumTitleLength { get; set; }
-        public int AreaOfBusiness { get; set; }
-        public int DefaultRelistDuration { get; set; }
-
-    }
+    /// <summary>
+    /// JSON object of a Fee item which is a child of Category
+    /// </summary>
     public class Fees
     {
         public double Bundle { get; set; }
@@ -60,52 +55,68 @@ namespace ProductConsoleApp
         public double Reserve { get; set; }
         public double Subtitle { get; set; }
         public double TenDays { get; set; }
-        public List<ListingFeeTier> ListingFeeTiers { get; set; }
+        public List<ListingFeeTier> ListingFeeTiers { get; set; }//List of ListingFeeTiers items contained in Fees
         public double SecondCategory { get; set; }
     }
+    /// <summary>
+    /// Create a json object of ListingFeeTier which is a child of Fee for complete creation of Category json object
+    /// </summary>
     public class ListingFeeTier
     {
         public int MinimumTierPrice { get; set; }
         public double FixedFee { get; set; }
     }
+    /// <summary>
+    /// Class with a main method as the entrypoint of the c# console application
+    /// </summary>
     public class Program
     {
+        //Instantiate a single Http client object that is used in all methods
+        public static HttpClient client = new HttpClient();
+
         static void Main(string[] args)
         {
 
         }
-
+        /// <summary>
+        /// This method returns the boolean status of CanRelist parameter in the Category json object 
+        /// </summary>
+        /// <returns>bool</returns>
         public static async Task<bool> GetCanRelistStatus()
         {
-            var client = new HttpClient();
+            //var client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync("https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false");
             if(response.IsSuccessStatusCode)
             {
-                var content = await response.Content.ReadAsAsync<ProductName>();
+                var content = await response.Content.ReadAsAsync<Category>();
                 return content.CanRelist;
             }
             return false;
         }
-
-        public static async Task<string> GetProductName()
+        /// <summary>
+        /// This method returns a string representation of the Category
+        /// </summary>
+        /// <returns>string</returns>
+        public static async Task<string> GetCategory()
         {
-            var client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync("https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false");
             if (response.IsSuccessStatusCode)
             {
-                var content = await response.Content.ReadAsAsync<ProductName>();
+                var content = await response.Content.ReadAsAsync<Category>();
                 return content.Name;
             }
             return null;
         }
-
+        /// <summary>
+        /// This method returns a List of JSON Promotion objects contained in Category
+        /// </summary>
+        /// <returns>List of Promotion JSON objects</returns>
         public static async Task<List<Promotion>> GetPromotionList()
             {
-            var client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync("https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false");
             if (response.IsSuccessStatusCode)
             {
-                var content = await response.Content.ReadAsAsync<ProductName>();
+                var content = await response.Content.ReadAsAsync<Category>();
                 List<Promotion> ListOfPromotions = new List<Promotion>();
                 foreach (Promotion promo in content.Promotions)
                 {
