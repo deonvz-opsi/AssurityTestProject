34b9972fa740aeaecb900387d16dee7a104468c4
Deon van Zyl <075deon@gmail.com>
2018-09-30 11:19:12 +0200

    Added string content to assertions, describing failure conditions

--- AssurityMSTestProject/AutomationTests.cs
+++ AssurityMSTestProject/AutomationTests.cs
@@ -25,16 +25,17 @@ namespace AssurityMSTestProject
             //Get the boolean value of Category CanRelist item
             bool CategoryCanRelist = Program.GetCanRelistStatus().GetAwaiter().GetResult();
 
-            //Verify the Name of the Category JSON object
+            //Verify the Name of the Category JSON object is "Carbon credits" else fail and report error
             Assert.IsTrue(CategoryName.Equals("Carbon credits"), "Product name is not equal to \"Carbon credits\"");
-            //Verify the boolean "true status" of the Category CanRelist status
-            Assert.IsTrue(CategoryCanRelist);
+            //Verify the boolean "true status" of the Category CanRelist status, else fail and report error
+            Assert.IsTrue(CategoryCanRelist, "CanRelist status is \"false\" and not \"true\" as expected");
             //Verify the string content "2x large image" in the Promotion item with Name "Gallery"
             foreach (var item in promotions)
             {
                 if (item.Name.Equals("Gallery"))
                 {
-                    Assert.IsTrue(item.Description.ToString().Contains("2x larger image"), "Gallery description varification has failed");
+                    Assert.IsTrue(item.Description.ToString().Contains("2x larger image"), 
+                        "Gallery description varification is not equal to \"2x large image\"");
                 }
             }
         }
@@ -50,7 +51,7 @@ namespace AssurityMSTestProject
         public void VerifyCanRelistTrue()
         {
             bool CategoryCanRelist = Program.GetCanRelistStatus().GetAwaiter().GetResult();
-            Assert.IsTrue(CategoryCanRelist);
+            Assert.IsTrue(CategoryCanRelist, "CanRelist status is \"false\" and not \"true\" as expected");
         }
         //Verify the string content "2x large image" in the Promotion item with Name "Gallery"
         [TestMethod]
@@ -61,7 +62,8 @@ namespace AssurityMSTestProject
             {
                 if(item.Name.Equals("Gallery"))
                 {
-                    Assert.IsTrue(item.Description.ToString().Contains("2x larger image"), "Gallery description varification has failed");
+                    Assert.IsTrue(item.Description.ToString().Contains("2x larger image"),
+                        "Gallery description varification is not equal to \"2x large image\"");
                 }
             }
         }
